295971aa4a1521112b5aa7a642d092af2f51a960
Alex Riviere <fimion@gmail.com>
2017-10-10 08:01:22 -0400

    added highlight js

--- src/components/Slide3.vue
+++ src/components/Slide3.vue
@@ -3,21 +3,51 @@
         <section>
             <h2>Basic Vue.js Usage</h2>
         </section>
+        <section>
+            <pre>
+                <code class="hljs" data-trim>
+                    &lt;div id="app"&gt;
+                            {{msgCode}}
+                    &lt;/div&gt;
+                    &lt;script&gt;
+                    var app = new Vue({
+                            el:"#app",
+                            data:{
+                                msg:"Hello, World!"
+                            }
+                      });
+                    &lt;/script&gt;
+                </code>
+            </pre>
+            <p class="fragment">
+                Result:<br/>
+                <code class="hljs">{{msg}}</code>
+            </p>
+        </section>
     </section>
 </template>
 
 <script>
+
+    import HLJS from 'highlight.js'
+
     export default {
         name: "slide-3",
         mounted() {
+          HLJS.initHighlighting();
         },
         data() {
-            return {};
+            return {
+              msgCode:"{{msg}}",
+              msg:"Hello, World!",
+            };
         },
-        methods: {}
+        methods: {},
+
     }
 </script>
 
 <style scoped>
+    @import '~highlight.js/styles/atom-one-dark.css';
 
 </style>
